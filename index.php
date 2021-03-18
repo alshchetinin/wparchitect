fa13a7104140b6587b3399ae9d70959091191fc7
alshchetinin <alexsander.qw@gmail.com>
2021-03-18 17:05:49 +0300

    first

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php get_header(); ?>
-  <p class="text-6xl">Сайты</p>
-  <p class="text-5xl">The quick brown fox jumps over the lazy dog.</p>
-  <p class="text-4xl">The quick brown fox jumps over the lazy dog.</p>
-  <p class="text-3xl">The quick brown fox jumps over the lazy dog.</p>
-  <p class="text-2xl">The quick brown fox jumps over the lazy dog.</p>
-  <p class="text-xl">The quick brown fox jumps over the lazy dog.</p>
-  <p class="text-lg">The quick brown fox jumps over the lazy dog.</p>
-  <p class="text-base">The quick brown fox jumps over the lazy dog.</p>
-  <p class="text-sm">The quick brown fox jumps over the lazy dog.</p>
-  <p class="text-xs">The quick brown fox jumps over the lazy dog.</p>
 
 <?php get_footer(); ?>
